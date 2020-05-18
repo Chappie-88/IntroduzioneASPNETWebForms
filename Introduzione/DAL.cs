dab5444cfeb3b71c37f2104014ef62c3c7ce3c5e
turtleseverywhere <s.samele@libero.it>
2020-05-18 23:28:30 +0200

    test

--- Introduzione/DAL.cs
+++ Introduzione/DAL.cs
@@ -8,7 +8,7 @@ using System.Web.Configuration;
 
 namespace Introduzione
 {
-    public static class DAL //data access layer
+    public static class DAL //data access layer//
     {
         public static void insertPerson(Person p)
         {
